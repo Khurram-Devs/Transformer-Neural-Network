ba80f1ed4b87a183316d7db643d282572c7d91d0
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-03 19:28:45 +0500

    coded self attention

---
+++ self_attention.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+L, d_k, d_v = 4, 8, 8
+q = np.random.randn(L, d_k)
+k = np.random.randn(L, d_k)
+v = np.random.randn(L, d_v)
+
+def softmax(x):
+    return (np.exp(x).T / np.sum(np.exp(x), axis=-1)).T
+
+def scaled_dot_product_attention(q, k, v, mask=None):
+    d_k = q.shape[-1]
+    scaled = np.matmul(q, k.T) / math.sqrt(d_k)
+    if mask is not None:
+        scaled = scaled + mask
+    attention = softmax(scaled)
+    out = np.matmul(attention, v)
+    return out, attention
+
+values, attention = scaled_dot_product_attention(q, k, v, mask=None)
+print("Q\n", q)
+print("K\n", k)
+print("V\n", v)
+print("New\n", values)
+print("Attention\n", attention)
