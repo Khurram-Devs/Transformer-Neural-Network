42e561f8c407d9cb06457246e937747008908607
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:41:11 +0500

    updated layer normalization
    fixed errors, removed prints

--- layer_normalization.py
+++ layer_normalization.py
@@ -12,15 +12,9 @@ class LayerNormalization(nn.Module):
 
     def forward(self, inputs):
         dims = [-(i + 1) for i in range(len(self.parameters_shape))]
-        print(f"dims: {dims}")
         mean = inputs.mean(dim=dims, keepdim=True)
-        print(f"Mean \n ({mean.size()})")
         var = ((inputs - mean) ** 2).mean(dim=dims, keepdim=True)
         std = (var + self.eps).sqrt()
-        print(f"Standard Deviation \n ({std.size()})")
         y = (inputs - mean) / std
-        print(f"y \n ({y.size()})")
         out = self.gamma * y + self.beta
-        print(f"self.gamma: {self.gamma.size()}, self.beta: {self.beta.size()}")
-        print(f"out \n ({out.size()})")
         return out
