44b38fe9d38ca8347245fe24d4fc9579c29cb053
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:45:09 +0500

    updated decoder layer
    fixed errors, removed prints

--- decoder_layer.py
+++ decoder_layer.py
@@ -10,41 +10,34 @@ class DecoderLayer(nn.Module):
     def __init__(self, d_model, ffn_hidden, num_heads, drop_prob):
         super(DecoderLayer, self).__init__()
         self.self_attention = MultiHeadAttention(d_model=d_model, num_heads=num_heads)
-        self.norm1 = LayerNormalization(parameters_shape=[d_model])
-        self.droFpout1 = nn.Dropout(p=drop_prob)
+        self.layer_norm1 = LayerNormalization(parameters_shape=[d_model])
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
         self.encoder_decoder_attention = MultiHeadCrossAttention(
             d_model=d_model, num_heads=num_heads
         )
-        self.norm2 = LayerNormalization(parameters_shape=[d_model])
+        self.layer_norm2 = LayerNormalization(parameters_shape=[d_model])
         self.dropout2 = nn.Dropout(p=drop_prob)
+
         self.ffn = PositionwiseFeedForward(
             d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob
         )
-        self.norm3 = LayerNormalization(parameters_shape=[d_model])
+        self.layer_norm3 = LayerNormalization(parameters_shape=[d_model])
         self.dropout3 = nn.Dropout(p=drop_prob)
 
-    def forward(self, x, y, decoder_mask):
-        _y = y
-        print("MASKED SELF ATTENTION")
-        y = self.self_attention(y, mask=decoder_mask)
-        print("DROP OUT 1")
+    def forward(self, x, y, self_attention_mask, cross_attention_mask):
+        _y = y.clone()
+        y = self.self_attention(y, mask=self_attention_mask)
         y = self.dropout1(y)
-        print("ADD + LAYER NORMALIZATION 1")
-        y = self.norm1(y + _y)
+        y = self.layer_norm1(y + _y)
 
-        _y = y
-        print("CROSS ATTENTION")
-        y = self.encoder_decoder_attention(x, y, mask=None)
-        print("DROP OUT 2")
+        _y = y.clone()
+        y = self.encoder_decoder_attention(x, y, mask=cross_attention_mask)
         y = self.dropout2(y)
-        print("ADD + LAYER NORMALIZATION 2")
-        y = self.norm2(y + _y)
+        y = self.layer_norm2(y + _y)
 
-        _y = y
-        print("FEED FORWARD 1")
+        _y = y.clone()
         y = self.ffn(y)
-        print("DROP OUT 3")
         y = self.dropout3(y)
-        print("ADD + LAYER NORMALIZATION 3")
-        y = self.norm3(y + _y)
+        y = self.layer_norm3(y + _y)
         return y
