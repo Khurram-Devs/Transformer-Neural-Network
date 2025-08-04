5a9bdc149e0c38ca27709e5d6425b90ea9334d6d
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:40:29 +0500

    updated multi head attention
    fixed errors, removed prints

--- mutli_head_attention.py
+++ mutli_head_attention.py
@@ -13,25 +13,17 @@ class MultiHeadAttention(nn.Module):
         self.qkv_layer = nn.Linear(d_model, 3 * d_model)
         self.linear_layer = nn.Linear(d_model, d_model)
 
-    def forward(self, x, mask=None):
+    def forward(self, x, mask):
         batch_size, max_sequence_length, d_model = x.size()
-        print(f"x.size(): {x.size()}")
         qkv = self.qkv_layer(x)
-        print(f"qkv.size(): {qkv.size()}")
         qkv = qkv.reshape(
             batch_size, max_sequence_length, self.num_heads, 3 * self.head_dim
         )
-        print(f"qkv.size(): {qkv.size()}")
         qkv = qkv.permute(0, 2, 1, 3)
-        print(f"qkv.size(): {qkv.size()}")
         q, k, v = qkv.chunk(3, dim=-1)
-        print(f"q size: {q.size()},k size: {k.size()},v size: {v.size()},")
         values, attention = scaled_dot_product(q, k, v, mask)
-        print(f"values.size(): {values.size()}, attention.size() {attention.size()}")
-        values = values.reshape(
+        values = values.permute(0, 2, 1, 3).reshape(
             batch_size, max_sequence_length, self.num_heads * self.head_dim
         )
-        print(f"values.size(): {values.size()}")
         out = self.linear_layer(values)
-        print(f"out.size(): {out.size()}")
         return out
