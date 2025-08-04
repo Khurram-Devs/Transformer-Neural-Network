6224abb88ece3f385b40d5d4176bacaea9734679
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:45:34 +0500

    updated sequential decoder
    fixed errors

--- sequential_decoder.py
+++ sequential_decoder.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class SequentialDecoder(nn.Sequential):
     def forward(self, *inputs):
-        x, y, mask = inputs
+        x, y, self_attention_mask, cross_attention_mask = inputs
         for module in self._modules.values():
-            y = module(x, y, mask)
+            y = module(x, y, self_attention_mask, cross_attention_mask)
         return y
