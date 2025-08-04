154f7c1a0af666eb105e679d1841c55947a11f36
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:31:50 +0500

    coded positionwise feed forward

---
+++ positionwise_feed_forward.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        print(f"x after first linear layer: {x.size()}")
+        x = self.relu(x)
+        print(f"x after activation: {x.size()}")
+        x = self.dropout(x)
+        print(f"x after dropout: {x.size()}")
+        x = self.linear2(x)
+        print(f"x after 2nd linear layer: {x.size()}")
+        return x
