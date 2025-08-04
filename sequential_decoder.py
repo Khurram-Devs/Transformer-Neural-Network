b6137953f3d0eb5b4718ed7d0d5e3e67ad835e26
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:35:29 +0500

    coded sequential decoder

---
+++ sequential_decoder.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+
+class SequentialDecoder(nn.Sequential):
+    def forward(self, *inputs):
+        x, y, mask = inputs
+        for module in self._modules.values():
+            y = module(x, y, mask)
+        return y
