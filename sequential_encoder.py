6ffe09c12a9f882fe70f9d2e3c1b259ee21f4a22
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-04 21:43:22 +0500

    coded sequential encoder

---
+++ sequential_encoder.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+
+class SequentialEncoder(nn.Sequential):
+    def forward(self, *inputs):
+        x, self_attention_mask = inputs
+        for module in self._modules.values():
+            x = module(x, self_attention_mask)
+        return x
