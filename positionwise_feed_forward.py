9d4eb96bd8f628d1f00c70b2cd3750fc37834625
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-07 08:45:36 +0500

    Update positionwise_feed_forward.py

--- positionwise_feed_forward.py
+++ positionwise_feed_forward.py
@@ -1,16 +1,16 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
 
 
 class PositionwiseFeedForward(nn.Module):
-    def __init__(self, d_model, hidden_dim, dropout=0.1):
+    def __init__(self, d_model: int, hidden_dim: int, dropout: float = 0.1):
         super().__init__()
         self.ffn = nn.Sequential(
             nn.Linear(d_model, hidden_dim),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim, d_model)
+            nn.Linear(hidden_dim, d_model),
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return self.ffn(x)
