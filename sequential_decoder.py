be6c32b841737140688851de5c4c5c213649e68d
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-07 08:48:09 +0500

    Update sequential_decoder.py

--- sequential_decoder.py
+++ sequential_decoder.py
@@ -1,10 +1,19 @@
-from torch import nn
+from torch import nn, Tensor
+from typing import Optional
+
+
 class SequentialDecoder(nn.Module):
-    def __init__(self, *layers):
+    def __init__(self, *layers: nn.Module):
         super().__init__()
         self.layers = nn.ModuleList(layers)
 
-    def forward(self, x, y, self_attention_mask=None, cross_attention_mask=None):
+    def forward(
+        self,
+        x: Tensor,
+        y: Tensor,
+        self_attention_mask: Optional[Tensor] = None,
+        cross_attention_mask: Optional[Tensor] = None,
+    ) -> Tensor:
         for layer in self.layers:
             y = layer(x, y, self_attention_mask, cross_attention_mask)
         return y
