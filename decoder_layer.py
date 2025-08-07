d8bd2ce505be8eeee62268ecfb8a5730caf8f46b
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-07 08:47:05 +0500

    Update decoder_layer.py

--- decoder_layer.py
+++ decoder_layer.py
@@ -1,5 +1,6 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
+from typing import Optional
 from layer_normalization import LayerNormalization
 from multi_head_attention import MultiHeadAttention
 from multi_head_cross_attention import MultiHeadCrossAttention
@@ -7,34 +8,42 @@ from positionwise_feed_forward import PositionwiseFeedForward
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, d_model, ffn_hidden, num_heads, drop_prob=0.1):
+    def __init__(
+        self, d_model: int, ffn_hidden: int, num_heads: int, drop_prob: float = 0.1
+    ):
         super().__init__()
         self.self_attention = MultiHeadAttention(d_model, num_heads)
-        self.norm1 = LayerNormalization(parameters_shape=[d_model])
+        self.norm1 = LayerNormalization([d_model])
         self.dropout1 = nn.Dropout(drop_prob)
 
         self.cross_attention = MultiHeadCrossAttention(d_model, num_heads)
-        self.norm2 = LayerNormalization(parameters_shape=[d_model])
+        self.norm2 = LayerNormalization([d_model])
         self.dropout2 = nn.Dropout(drop_prob)
 
-        self.ffn = PositionwiseFeedForward(d_model, hidden_dim=ffn_hidden, dropout=drop_prob)
-        self.norm3 = LayerNormalization(parameters_shape=[d_model])
+        self.ffn = PositionwiseFeedForward(d_model, ffn_hidden, drop_prob)
+        self.norm3 = LayerNormalization([d_model])
         self.dropout3 = nn.Dropout(drop_prob)
 
-    def forward(self, encoder_output, decoder_input, self_attention_mask=None, cross_attention_mask=None):
-        residual1 = decoder_input
+    def forward(
+        self,
+        encoder_output: Tensor,
+        decoder_input: Tensor,
+        self_attention_mask: Optional[Tensor] = None,
+        cross_attention_mask: Optional[Tensor] = None,
+    ) -> Tensor:
+        res1 = decoder_input
         x = self.self_attention(decoder_input, mask=self_attention_mask)
         x = self.dropout1(x)
-        x = self.norm1(x + residual1)
+        x = self.norm1(x + res1)
 
-        residual2 = x
+        res2 = x
         x = self.cross_attention(encoder_output, x, mask=cross_attention_mask)
         x = self.dropout2(x)
-        x = self.norm2(x + residual2)
+        x = self.norm2(x + res2)
 
-        residual3 = x
+        res3 = x
         x = self.ffn(x)
         x = self.dropout3(x)
-        x = self.norm3(x + residual3)
+        x = self.norm3(x + res3)
 
         return x
