39a3b28501e529714e02a1564d8358431743f57c
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-07 08:45:55 +0500

    Update multi_head_attention.py

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 from global_functions import scaled_dot_product
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model: int, num_heads: int):
         super().__init__()
-        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        assert d_model % num_heads == 0
         self.d_model = d_model
         self.num_heads = num_heads
         self.head_dim = d_model // num_heads
@@ -14,15 +15,17 @@ class MultiHeadAttention(nn.Module):
         self.qkv_layer = nn.Linear(d_model, 3 * d_model)
         self.output_layer = nn.Linear(d_model, d_model)
 
-    def forward(self, x, mask=None):
-        batch_size, seq_len, _ = x.size()
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
+        b, t, _ = x.size()
 
         qkv = self.qkv_layer(x)
-        qkv = qkv.view(batch_size, seq_len, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.view(b, t, self.num_heads, 3 * self.head_dim)
         qkv = qkv.permute(0, 2, 1, 3)
         q, k, v = qkv.chunk(3, dim=-1)
-        context, attention = scaled_dot_product(q, k, v, mask=mask)
+
+        context, _ = scaled_dot_product(q, k, v, mask)
+
         context = context.permute(0, 2, 1, 3).contiguous()
-        context = context.view(batch_size, seq_len, self.d_model)
-        output = self.output_layer(context)
-        return output
+        context = context.view(b, t, self.d_model)
+
+        return self.output_layer(context)
