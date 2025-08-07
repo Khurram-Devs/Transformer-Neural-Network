da98bf4f869217a71aeef951527d875ac400cc6c
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-07 08:48:34 +0500

    Update sequential_encoder.py

--- sequential_encoder.py
+++ sequential_encoder.py
@@ -1,10 +1,15 @@
-from torch import nn
+from torch import nn, Tensor
+from typing import Optional
+
+
 class SequentialEncoder(nn.Module):
-    def __init__(self, *layers):
+    def __init__(self, *layers: nn.Module):
         super().__init__()
         self.layers = nn.ModuleList(layers)
 
-    def forward(self, x, self_attention_mask):
+    def forward(
+        self, x: Tensor, self_attention_mask: Optional[Tensor] = None
+    ) -> Tensor:
         for layer in self.layers:
             x = layer(x, self_attention_mask)
         return x
