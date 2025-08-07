188bc9c139b58e1781a10742c3ebff81840aadac
Khurram Iqbal <ikhurram765@gmail.com>
2025-08-07 08:46:26 +0500

    Update multi_head_cross_attention.py

--- multi_head_cross_attention.py
+++ multi_head_cross_attention.py
@@ -1,12 +1,12 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
 from global_functions import scaled_dot_product
 
 
 class MultiHeadCrossAttention(nn.Module):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model: int, num_heads: int):
         super().__init__()
-        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        assert d_model % num_heads == 0
 
         self.d_model = d_model
         self.num_heads = num_heads
@@ -16,24 +16,22 @@ class MultiHeadCrossAttention(nn.Module):
         self.q_layer = nn.Linear(d_model, d_model)
         self.output_layer = nn.Linear(d_model, d_model)
 
-    def forward(self, x, y, mask=None):
-        batch_size, src_seq_len, _ = x.size()
-        _, tgt_seq_len, _ = y.size()
+    def forward(self, x: Tensor, y: Tensor, mask: Tensor = None) -> Tensor:
+        b, s_len, _ = x.size()
+        _, t_len, _ = y.size()
 
-        kv = self.kv_layer(x) 
-        kv = kv.view(batch_size, src_seq_len, self.num_heads, 2 * self.head_dim)
+        kv = self.kv_layer(x)
+        kv = kv.view(b, s_len, self.num_heads, 2 * self.head_dim)
         kv = kv.permute(0, 2, 1, 3)
-        k, v = kv.chunk(2, dim=-1) 
+        k, v = kv.chunk(2, dim=-1)
 
         q = self.q_layer(y)
-        q = q.view(batch_size, tgt_seq_len, self.num_heads, self.head_dim)
+        q = q.view(b, t_len, self.num_heads, self.head_dim)
         q = q.permute(0, 2, 1, 3)
 
-        context, attention = scaled_dot_product(q, k, v, mask=mask)  
+        context, _ = scaled_dot_product(q, k, v, mask)
 
         context = context.permute(0, 2, 1, 3).contiguous()
-        context = context.view(batch_size, tgt_seq_len, self.d_model)
+        context = context.view(b, t_len, self.d_model)
 
-        output = self.output_layer(context)
-
-        return output
+        return self.output_layer(context)
